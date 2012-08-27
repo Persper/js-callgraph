f3ef13c797b45348eeeaf00053db367d18647c5f
Max Schaefer <mschaefer@us.ibm.com>
2012-08-26 23:50:58 -0400

    Optimisation to TC computation: adjacency lists are now ordered, so we can merge them more efficiently.

--- graph.js
+++ graph.js
@@ -15,6 +15,23 @@ define(function(require, exports) {
     id2node[id] = nd;
     return id;
   }
+
+  function insert(a, x) {
+      var lo = 0, hi = a.length-1, mid, elt;
+      while(lo <= hi) {
+        mid = (lo+hi) >> 1;
+        elt = a[mid];
+        if(elt < x) {
+          lo = mid+1;
+        } else if(elt > x) {
+          hi = mid-1;
+        } else {
+          return a;
+        }
+      }
+      a.splice(lo, 0, x);
+      return a;
+  }
   
   function addSucc(succ, toId) {
     if(typeof succ === 'undefined') {
@@ -25,19 +42,7 @@ define(function(require, exports) {
       else if(succ > toId)
         succ = [toId, succ];
     } else {
-      var lo = 0, hi = succ.length-1, mid, elt;
-      while(lo <= hi) {
-        mid = (lo+hi) >> 1;
-        elt = succ[mid];
-        if(elt < toId) {
-          lo = mid+1;
-        } else if(elt > toId) {
-          hi = mid-1;
-        } else {
-          return succ;
-        }
-      }
-      succ.splice(lo, 0, toId);
+      insert(succ, toId);
     }    
     return succ;
   }
@@ -83,6 +88,59 @@ define(function(require, exports) {
     }
   };
 
+  function merge(a, b) {
+    if(typeof a === 'number') {
+      if(typeof b === 'number') {
+        if(a < b)
+          return [a, b];
+        else if(a > b)
+          return [b, a];
+        return a;
+      } else {
+        return merge(b, a);
+      }
+    }
+
+    // 'a' must be an array; check 'b'
+    if(typeof b === 'number') {
+      var l = a.length;
+
+      if(l == 0)
+        return b;
+
+      var res = new Array(l+1);
+      var i = 0, k = 0;
+      while(i < l && a[i] <= b)
+        res[k++] = a[i++];
+      if(b !== res[k-1])
+        res[k++] = b;
+      while(i < l)
+        res[k++] = a[i++];
+      res.length = k;
+      return res;
+    } else {
+      var l1 = a.length, l2 = b.length;
+
+      if(l1 === 0)
+        return b;
+      if(l2 === 0)
+        return a;
+
+      var res = new Array(l1+l2);
+      var i = 0, j = 0, k = 0;
+      while(i < l1 || j < l2) {
+        while(i < l1 && (j >= l2 || a[i] <= b[j]))
+          res[k++] = a[i++];
+        while(k > 0 && j < l2 && b[j] === res[k-1])
+          ++j;
+        while(j < l2 && (i >= l1 || b[j] < a[i]))
+          res[k++] = b[j++];
+      }
+      res.length = k;
+      return res;
+    }
+  }
+
   Graph.prototype.reachability = function(nodePred) {
     var self = this;
     var tc = [];
@@ -91,38 +149,41 @@ define(function(require, exports) {
       if(src_id in tc)
         return tc[src_id];
 
-      tc[src_id] = [src_id];
+      tc[src_id] = src_id;
       if(nodePred && !nodePred(id2node[src_id]))
         return tc[src_id];
 
       var succ = self.succ[src_id];
+      var new_tc = tc[src_id];
       if(typeof succ === 'number') {
         var succ_succ = computeTC(succ);
-        for(var j=0;j<succ_succ.length;++j)
-          if(tc[src_id].indexOf(succ_succ[j]) === -1)
-            tc[src_id].push(succ_succ[j]);
+        new_tc = merge(new_tc, succ_succ);
       } else if(succ && typeof succ === 'object') {
         for(var i=0;i<succ.length;++i) {
           var succ_succ = computeTC(succ[i]);
-          for(var j=0;j<succ_succ.length;++j)
-            if(tc[src_id].indexOf(succ_succ[j]) === -1)
-              tc[src_id].push(succ_succ[j]);
+          new_tc = merge(new_tc, succ_succ);
         }
       }
-      return tc[src_id];
+      return tc[src_id] = new_tc;
     }
 
     return {
       getReachable: function(src) {
         var src_id = nodeId(src);
         computeTC(src_id);
-        return tc[src_id].map(function(dest_id) { return id2node[dest_id]; });
+        var reachable = tc[src_id];
+        if(typeof reachable === 'number')
+          return [id2node[reachable]];
+        return reachable.map(function(dest_id) { return id2node[dest_id]; });
       },
       reaches: function(src, dest) {
         var src_id = nodeId(src), dest_id = nodeId(dest);
         computeTC(src_id);
-        for(var i=0;i<tc[src_id].length;++i)
-          if(tc[src_id][i] === dest_id)
+        var reachable = tc[src_id];
+        if(typeof reachable === 'number')
+          return reachable === dest_id;
+        for(var i=0;i<reachable.length;++i)
+          if(reachable[i] === dest_id)
             return true;
         return false;
       }
