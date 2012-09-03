3d7b806e8032bf912dce5b9ff6e16b9cffa1a70e
Max Schaefer <mschaefer@us.ibm.com>
2012-09-03 13:54:03 -0400

    Spinning out transitive closure computation into separate module.

--- graph.js
+++ graph.js
@@ -9,9 +9,9 @@ define(function(require, exports) {
     this.succ = [];
   };
   
-  var id2node = [];
+  var id2node = Graph.prototype.id2node = [];
   var nextNodeId = 1;
-  function nodeId(nd) {
+  var nodeId = Graph.prototype.nodeId = function nodeId(nd) {
     var id = nd.attr.hasOwnProperty('node_id') ? nd.attr.node_id 
                                                : (nd.attr.node_id = nextNodeId++);
     id2node[+id] = nd;
@@ -30,16 +30,6 @@ define(function(require, exports) {
       this.addEdge(from, tos[i]);
   };
   
-  Graph.prototype.invert = function() {
-    var inv = new Graph();
-    this.succ.forEach(function(succs, i) {
-      numset.iter(succs, function(succ) {
-        inv.succ[succ] = numset.add(inv.succ[succ], i);
-      });
-    });
-    return inv;
-  };
-  
   Graph.prototype.iter = function(cb) {
     for(var i=0;i<this.succ.length;++i) {
       if(!this.succ[i])
@@ -56,49 +46,6 @@ define(function(require, exports) {
     return numset.contains(this.succ[fromId], toId);    
   }
 
-  Graph.prototype.reachability = function(nodePred) {
-    var self = this;
-    var tc = [], touched = [];
-
-    function computeTC(src_id) {
-      if(src_id in tc) {
-        touched[src_id] = true;
-        return tc[src_id];
-      }
-
-      tc[src_id] = src_id;
-      if(nodePred && !nodePred(id2node[src_id]))
-        return tc[src_id];
-
-      do {
-        var oldsz = numset.size(tc[src_id]);
-        touched[src_id] = false;
-        numset.iter(self.succ[src_id], function(succ) {
-          tc[src_id] = numset.addAll(tc[src_id], computeTC(succ));
-        });
-      } while(oldsz < numset.size(tc[src_id]) && touched[src_id]);
-      return tc[src_id];
-    }
-
-    return {
-      getReachable: function(src) {
-        var src_id = nodeId(src);
-        computeTC(src_id);
-        return numset.map(tc[src_id], function(dest_id) { return id2node[dest_id]; });
-      },
-      iterReachable: function(src, cb) {
-        var src_id = nodeId(src);
-        computeTC(src_id);
-        numset.iter(tc[src_id], function(dest_id) { cb(id2node[dest_id]); });
-      },
-      reaches: function(src, dest) {
-        var src_id = nodeId(src), dest_id = nodeId(dest);
-        computeTC(src_id);
-        return numset.some(tc[src_id], function(id) { return id === dest_id; });
-      }
-    };
-  };
-  
   exports.Graph = Graph;
   return exports;
 });
