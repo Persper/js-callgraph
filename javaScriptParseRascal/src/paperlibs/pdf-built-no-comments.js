3c1d99bf4ad9b0e8f870ba8321ed8409ee1b4a9f
Sander Benschop <sander1990@gmail.com>
2014-03-09 14:40:59 +0100

    manually fixed syntax error

--- javaScriptParseRascal/src/paperlibs/pdf-built-no-comments.js
+++ javaScriptParseRascal/src/paperlibs/pdf-built-no-comments.js
@@ -9,8 +9,7 @@ if (typeof PDFJS === 'undefined') {
 }
 
 PDFJS.version = '0.8.2';
-PDFJS.build = 'fatal: Not a git repository (or any parent up to mount point /home/sander)Stopping at filesystem boundary (GIT_DISCOVERY_ACROSS_FILESYSTEM not set).
-';
+PDFJS.build = 'fatal: Not a git repository (or any parent up to mount point /home/sander)Stopping at filesystem boundary (GIT_DISCOVERY_ACROSS_FILESYSTEM not set).';
 
 (function pdfjsWrapper() {
   
--- javaScriptParseRascal/src/snippets/multiLineSingleQuoteString.js
+++
@@ -1,2 +0,0 @@
-var message = 'fatal: Not a git repository (or any parent up to mount point /home/sander)Stopping at filesystem boundary (GIT_DISCOVERY_ACROSS_FILESYSTEM not set).
-';
