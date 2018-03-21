7af1660ac5c7d0747899df821f5f9f1c9bd9d213
alexstennet <alexstennet@berkeley.edu>
2018-03-21 14:28:07 -0700

    Add .gitignore and updated esprima
    To run the program after downloading repo, you must run:
    `npm install`
    This should install all dependencies for the project.

--- astutil.js
+++ astutil.js
@@ -16,7 +16,7 @@ if (typeof define !== 'function') {
 }
 
 define(function (require, exports) {
-    var esprima = require('./esprima');
+    var esprima = require('esprima');
     var fs = require('fs');
     var sloc  = require('sloc');
 
