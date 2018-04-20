d1f6d0776ab44658fe07b188edcff9cf6f3c93fc
Alex Stennet <alex@persper.org>
2018-04-20 13:50:39 -0700

    Add better printing and clean remove commented out code

--- tests/process.py
+++ tests/process.py
@@ -28,27 +28,20 @@ assert len(sys.argv) > 1, "Incorrect number of arguments: process.py FILENAME <T
 # must be included to fill the callgraph
 required_files = collect_requires(sys.argv[1])
 strd_files = [str(rf) for rf in required_files]
-for rf in strd_files:
-    print(rf)
 
 # Run the javascript call graph program and capture the output
 output = callgraph(strd_files)
 lines = str(output)[2:].split('\\n')
-# for line in lines:
-#     print(line)
 
 # The output will contain lines for all files that were required
 # This will filter out so only one's relevant to the file selected are shown
 reg_input_file_name = re.compile(r'.*\(' + required_files[0].name + r'@[0-9]*:[0-9]*-[0-9]*\) ->.*')
 filtered_out = [line for line in lines if reg_input_file_name.match(line)]
-# print(len(lines))
+
 # Format output to align with the expected output
 fo = []
 for f in filtered_out:
     fo.append(format_output(f))
-
-# print(fo)
-# print(len(fo))
 
 # Reading in expected output file and comparing with output
 if len(sys.argv) >= 3:
@@ -56,27 +49,37 @@ if len(sys.argv) >= 3:
 
     lines = [line[:-1] for line in f]
 
-    count1 = len([l for l in lines if l in fo])
-    count2 = len([l for l in fo if l in lines])
+    output_lines = set(fo)
+    expected_lines = set(lines)
 
-    wo_native1 = [l for l in lines if 'Native' not in l and '_interopRequireDefault' not in l and l[-2:-1] != '->']
-    wo_native2 = [l for l in fo if 'Native' not in l and '_interopRequireDefault' not in l and l[-2:-1] != '->']
+    intersection = output_lines & expected_lines
 
-    count3 = len([l for l in wo_native1 if l in wo_native2])
-    count4 = len([l for l in wo_native2 if l in wo_native1])
+    output_no_natives = [l for l in fo if 'Native' not in l and
+                                   '_interopRequireDefault' not in l and
+                                   l[-2:-1] != '->']
+    expected_no_natives = [l for l in lines if 'Native' not in l and
+                                      '_interopRequireDefault' not in l and
+                                      l[-2:-1] != '->']
 
-    print(count1, len(fo))
-    print(count2, len(lines))
+    output_set_wo_natives = set(output_no_natives)
+    expected_set_wo_natives = set(expected_no_natives)
 
-    # print(wo_native1)
-    # print(wo_native2)
-    print(count3, len(wo_native2))
-    print(count4, len(wo_native1))
+    interesection_wo_natives = output_set_wo_natives & expected_set_wo_natives
 
     print('Actual Output (w/o natives)')
-    for f in wo_native2:
+    for f in output_no_natives:
         print(f)
-
-    print('\nExpected Output (w/o natives)')
-    for f in wo_native1:
+    print()
+    print('Expected Output (w/o natives)')
+    for f in expected_no_natives:
         print(f)
+    print()
+    print('INCLUDING NATIVES')
+    print('Precision:', 100*len(intersection) // len(output_lines), '% (', len(intersection), '/', len(output_lines), ')')
+    print('Recall: ', 100*len(intersection) // len(expected_lines), '% (', len(intersection), '/', len(expected_lines), ')')
+    print()
+    print('EXCLUDING NATIVES')
+    print('Precision:', 100*len(interesection_wo_natives) // len(output_set_wo_natives),
+          '% (', len(interesection_wo_natives), '/', len(output_set_wo_natives), ')')
+    print('Recall: ', 100*len(interesection_wo_natives) // len(expected_set_wo_natives),
+          '% (', len(interesection_wo_natives), '/', len(expected_set_wo_natives), ')')
