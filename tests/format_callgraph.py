13a26a97404d069e2c44d31f028669f126683f14
alexstennet <alexstennet@berkeley.edu>
2018-04-20 12:49:37 -0700

    Change testing framwork
    Now works inside repo and should align with the current output scheme
    for the call graph. Additionally, separated the testing framework into
    multiple files to increase readbility.

---
+++ tests/format_callgraph.py
@@ -0,0 +1,39 @@
+import re
+
+# Regex for representing the call graph itself
+call_func = r'\'(.*)\' \(([^1-9]*)@([0-9]*):[0-9]*-[0-9]*\)'
+native = r'\'(.*)\' \(Native\)'
+
+reg_func = re.compile(call_func + ' -> ' + call_func)
+reg_native = re.compile(call_func + ' -> ' + native)
+
+def format_func(out):
+    m = reg_func.match(out)
+
+    fn_name1 = m.group(1)
+    file1 = m.group(2)
+    line1 = m.group(3)
+
+    fn_name2 = m.group(4)
+    file2 = m.group(5)
+    line2 = m.group(6)
+
+    return '{0}:{1}:{2} -> {3}:{4}:{5}'.format(file1, fn_name1, line1, file2, fn_name2, line2)
+
+def format_native(out):
+    m = reg_native.match(out)
+
+    fn_name1 = m.group(1)
+    file1 = m.group(2)
+    line1 = m.group(3)
+
+    native_name = m.group(4)
+
+    return '{0}:{1}:{2} -> Native'.format(file1, fn_name1, line1)
+
+def format_output(out):
+    # print(out)
+    if 'Native' in out:
+        return format_native(out)
+    else:
+        return format_func(out)
--- tests/process.py
+++ tests/process.py
@@ -3,112 +3,50 @@
 import sys
 from pathlib import Path
 import re
-import subprocess
-from resolve_path import resolve_path
+from required_files import collect_requires
+from resolve_path import resolve_path, root
+from callgraph import callgraph
+from format_callgraph import format_output
 
-assert len(sys.argv) > 2, "Incorrect number of arguments: process.py FILENAME ROOT_PATH <TEST_FILE>"
+assert len(sys.argv) > 1, "Incorrect number of arguments: process.py FILENAME <TEST_FILE>"
 
-reg = re.compile("require\(\'(.*)\'\)")
+# Recursively descend through require's to find all files that
+# must be included to fill the callgraph
+required_files = collect_requires(sys.argv[1])
+strd_files = [str(rf) for rf in required_files]
+for rf in strd_files:
+    print(rf)
 
-def get_requires(path):
-    try:
-        f = path.open()
-        text = f.read()
-        f.close()
-        matches = reg.findall(text)
-        return [m + '.js.o' for m in matches]
-    except:
-        return []
+# Run the javascript call graph program and capture the output
+output = callgraph(strd_files)
+lines = str(output)[2:].split('\\n')
+# for line in lines:
+#     print(line)
 
-read_files = []
-unread_files = [resolve_path(sys.argv[1], root)]
-
-while unread_files:
-    uf = unread_files.pop(0)
-    print(uf)
-
-    if uf in read_files:
-        continue
-
-    read_files.append(uf.resolve())
-
-    required_files = get_requires(uf)
-
-    for rf in required_files:
-        rp = resolve_path(rf, uf.parent)
-        if rp:
-            unread_files.append(rp.resolve())
-
-strd_files = [str(rf) for rf in read_files]
-
-node_program = '../src'
-
-program = ['node', node_program, '--cg', *strd_files]
-process = subprocess.run(program, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-out = process.stdout
-
-lines = str(out)[2:].split('\\n')
-
-# print(lines[:10])
-
-reg2 = re.compile('.*\(' + read_files[0].name + '@.*\) ->.*')
-
-filtered_out = [line for line in lines if reg2.match(line)]
-# print(filtered_out)
-# for f in filtered_out:
-#     print(f)
-
-reg_func = re.compile(r'.*\(([^1-9]*)@([0-9]*):[0-9]*-[0-9]*\) -> .* \'(.*)\' \(([^1-9]*)@([0-9]*):[0-9]*-[0-9]*\)')
-reg_native = re.compile(r'.*\(([^1-9]*)@([0-9]*):[0-9]*-[0-9]*\) -> NativeVertex (.*)')
-
-def format_func(out):
-    m = reg_func.match(out)
-
-    file1 = m.group(1)
-    line1 = m.group(2)
-
-    fn_name = m.group(3)
-
-    file2 = m.group(4)
-    line2 = m.group(5)
-
-    return '{0}:{1} -> {3}:{2}:{4}'.format(file1, line1, fn_name, file2, line2)
-
-def format_native(out):
-    m = reg_native.match(out)
-
-    file1 = m.group(1)
-    line1 = m.group(2)
-
-    native_name = m.group(3)
-
-    return '{0}:{1} -> NativeVertex'.format(file1, line1)
-
-def format_output(out):
-    # print(out)
-    if 'NativeVertex' in out:
-        return format_native(out)
-    else:
-        return format_func(out)
-
+# The output will contain lines for all files that were required
+# This will filter out so only one's relevant to the file selected are shown
+reg_input_file_name = re.compile(r'.*\(' + required_files[0].name + r'@[0-9]*:[0-9]*-[0-9]*\) ->.*')
+filtered_out = [line for line in lines if reg_input_file_name.match(line)]
+# print(len(lines))
+# Format output to align with the expected output
 fo = []
-
 for f in filtered_out:
     fo.append(format_output(f))
 
-if len(sys.argv) >= 4:
-    f = open(sys.argv[3])
+# print(fo)
+# print(len(fo))
+
+# Reading in expected output file and comparing with output
+if len(sys.argv) >= 3:
+    f = open(sys.argv[2])
 
     lines = [line[:-1] for line in f]
-    print(lines)
-    print(fo)
 
     count1 = len([l for l in lines if l in fo])
     count2 = len([l for l in fo if l in lines])
 
-    wo_native1 = [l for l in lines if 'Native' not in l and '_interopRequireDefault' not in l]
-    wo_native2 = [l for l in fo if 'Native' not in l and '_interopRequireDefault' not in l]
+    wo_native1 = [l for l in lines if 'Native' not in l and '_interopRequireDefault' not in l and l[-2:-1] != '->']
+    wo_native2 = [l for l in fo if 'Native' not in l and '_interopRequireDefault' not in l and l[-2:-1] != '->']
 
     count3 = len([l for l in wo_native1 if l in wo_native2])
     count4 = len([l for l in wo_native2 if l in wo_native1])
@@ -116,10 +54,15 @@ if len(sys.argv) >= 4:
     print(count1, len(fo))
     print(count2, len(lines))
 
-    print(wo_native1)
-    print(wo_native2)
+    # print(wo_native1)
+    # print(wo_native2)
     print(count3, len(wo_native2))
     print(count4, len(wo_native1))
 
-for f in wo_native2:
-    print(f)
+    print('Actual Output (w/o natives)')
+    for f in wo_native2:
+        print(f)
+
+    print('\nExpected Output (w/o natives)')
+    for f in wo_native1:
+        print(f)
