303efb487c8205f1e0150830bda707fd1f7e6872
Hezheng Yin <hezheng@persper.org>
2018-04-23 15:03:09 -0700

    Move require2.js to unexpected folder and rename test cases

--- tests/basics/require.js
+++ tests/basics/require.js
@@ -1,3 +1,3 @@
 // This file tests the most common usage of exports/require
-let m1 = require('./module1');
+let m1 = require('./require-module');
 console.log("Adding %d to 10 gives us %d", m1.x, m1.addX(10));
