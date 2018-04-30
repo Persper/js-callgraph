49f97c42a818856c098ff6f43e09e1128883441d
Alex Stennet <alex@persper.org>
2018-04-30 10:51:22 -0700

    Add class based test

---
+++ tests/classes/class.js
@@ -0,0 +1,18 @@
+class Rectangle {
+  constructor(height, width) {
+    this.height = height;
+    this.width = width;
+  }
+  // Getter
+  get area() {
+    return this.calcArea();
+  }
+  // Method
+  calcArea() {
+    return this.height * this.width;
+  }
+}
+
+const square = new Rectangle(10, 10);
+
+console.log(square.area);
