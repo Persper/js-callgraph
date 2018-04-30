e89598b88febac659094eea049e9f021c3b89665
alexstennet <alexstennet@berkeley.edu>
2018-04-30 12:41:03 -0700

    Add a second class test to demonstrate methods not working.

---
+++ tests/classes/class-compiled2.js
@@ -0,0 +1,37 @@
+"use strict";
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Rectangle = function () {
+  function Rectangle(height, width) {
+    _classCallCheck(this, Rectangle);
+
+    this.height = height;
+    this.width = width;
+  }
+  // Getter
+
+
+  _createClass(Rectangle, [{
+    key: "calcArea",
+
+    // Method
+    value: function calcArea() {
+      return this.height * this.width;
+    }
+  }, {
+    key: "area",
+    get: function get() {
+      return this.calcArea();
+    }
+  }]);
+
+  return Rectangle;
+}();
+
+var square = new Rectangle(10, 10);
+
+console.log(square.calcArea());
+
