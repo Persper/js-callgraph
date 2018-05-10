3bdb0a1c3c43a5fae1271128da92f58fc78db511
Hezheng Yin <yinhezheng1995@gmail.com>
2018-05-09 17:42:55 -0700

    Use strategy DEMAND as default for tests

--- tests/callgraph.py
+++ tests/callgraph.py
@@ -3,6 +3,6 @@ import subprocess
 node_program = '../src/main.js'
 
 def callgraph(files):
-    program = ['node', node_program, '--cg', *files]
+    program = ['node', node_program, '--cg', *files, '--strategy', 'DEMAND']
     process = subprocess.run(program, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     return process.stdout
