cc22662882d083dc7aa59d1273a579619a602369
Hezheng Yin <hezheng@persper.org>
2018-05-24 03:32:24 -0700

    Refactor semioptimistic

--- src/semioptimistic.js
+++ src/semioptimistic.js
@@ -23,10 +23,8 @@ define(function (require, exports) {
         flowgraph = require('./flowgraph'),
         callgraph = require('./callgraph');
 
-    function buildCallGraph(ast) {
-        var fg = new graph.Graph();
-        natives.addNativeFlowEdges(fg);
-        flowgraph.addIntraproceduralFlowGraphEdges(ast, fg);
+    function addInterproceduralFlowEdges(ast, fg) {
+        fg = fg || new graph.Graph();
 
         var changed;
         do {
@@ -93,9 +91,86 @@ define(function (require, exports) {
             });
         } while (changed); // until fixpoint
 
+        return fg;
+    }
+
+    function buildCallGraph(ast) {
+        var fg = new graph.Graph();
+        natives.addNativeFlowEdges(fg);
+        flowgraph.addIntraproceduralFlowGraphEdges(ast, fg);
+        addInterproceduralFlowEdges(ast, fg);
+
+        /*
+        var changed;
+        do {
+            changed = false;
+
+            var reach = fg.reachability(function (nd) {
+                return nd.type !== 'UnknownVertex';
+            });
+
+            ast.attr.calls.forEach(function (call) {
+                var res = flowgraph.resVertex(call);
+                if (!res.attr.interesting)
+                    reach.iterReachable(res, function (nd) {
+                        if (nd.type === 'CalleeVertex') {
+                            //console.log(res.attr.pp() + " is interesting");
+                            res.attr.interesting = true;
+                        }
+                    });
+            });
+
+            ast.attr.functions.forEach(function (fn) {
+                var interesting = false, nparams = fn.params.length;
+
+                for (var i = 0; i <= nparams; ++i) {
+                    var param = flowgraph.parmVertex(fn, i);
+                    if (!param.attr.interesting) {
+                        reach.iterReachable(param, function (nd) {
+                            if (nd.type === 'CalleeVertex') {
+                                param.attr.interesting = true;
+                            }
+                        });
+                    }
+                    interesting = interesting || param.attr.interesting;
+                }
+
+                reach.iterReachable(flowgraph.funcVertex(fn), function (nd) {
+                    if (nd.type === 'CalleeVertex') {
+                        var call = nd.call, res = flowgraph.resVertex(call);
+
+                        if (res.attr.interesting) {
+                            var ret = flowgraph.retVertex(fn);
+                            if (!fg.hasEdge(ret, res)) {
+                                changed = true;
+                                fg.addEdge(ret, res);
+                            }
+                        }
+
+                        if (interesting)
+                            for (var i = 0; i <= nparams; ++i) {
+                                if (i > call.arguments.length)
+                                    break;
+
+                                var param = flowgraph.parmVertex(fn, i);
+                                if (param.attr.interesting) {
+                                    var arg = flowgraph.argVertex(call, i);
+                                    if (!fg.hasEdge(arg, param)) {
+                                        changed = true;
+                                        fg.addEdge(arg, param);
+                                    }
+                                }
+                            }
+                    }
+                });
+            });
+        } while (changed); // until fixpoint
+        */
+
         return callgraph.extractCG(ast, fg);
     }
 
+    exports.addInterproceduralFlowEdges = addInterproceduralFlowEdges;
     exports.buildCallGraph = buildCallGraph;
     return exports;
 });
