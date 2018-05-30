ba983dbeeb8a71b8d084deccac7288c4689b2633
Alex Stennet <alex@persper.org>
2018-05-29 23:23:05 -0700

    Add comments to bcolors.py, callgraph.py and format_callgraph.py

--- tests/callgraph.py
+++ tests/callgraph.py
@@ -3,6 +3,7 @@ import subprocess
 node_program = '../src/main.js'
 
 def callgraph(files):
+    """Run the primary callgraph generator and return its standard output."""
     program = ['node', node_program, '--cg', *files, '--strategy', 'DEMAND']
     process = subprocess.run(program, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     return process.stdout
--- tests/format_callgraph.py
+++ tests/format_callgraph.py
@@ -1,3 +1,6 @@
+# Summary: Methods for converting the callgraph output
+#          into truth file output
+
 import re
 
 # Regex for representing the call graph itself
@@ -8,6 +11,11 @@ reg_func = re.compile(call_func + ' -> ' + call_func)
 reg_native = re.compile(call_func + ' -> ' + native)
 
 def format_func(out):
+    """Convert function call to truth file format.
+
+    >>> format_output("'global' (basic-class2.js@8:55-67) -> 'grow' (basic-class2.js@2:21-30)")
+    'basic-class2.js:global:8 -> basic-class2.js:grow:2'
+    """
     m = reg_func.match(out)
 
     fn_name1 = m.group(1)
@@ -21,6 +29,11 @@ def format_func(out):
     return '{0}:{1}:{2} -> {3}:{4}:{5}'.format(file1, fn_name1, line1, file2, fn_name2, line2)
 
 def format_native(out):
+    """Convert native call to truth file format
+
+    >>> format_output("'global' (require.js@3:98-159) -> 'console_log' (Native)")
+    'require.js:global:3 -> Native'
+    """
     m = reg_native.match(out)
 
     fn_name1 = m.group(1)
@@ -32,7 +45,13 @@ def format_native(out):
     return '{0}:{1}:{2} -> Native'.format(file1, fn_name1, line1)
 
 def format_output(out):
-    # print(out)
+    """Convert either a function call or a native call to truth file format
+
+    >>> format_output("'global' (basic-class2.js@8:55-67) -> 'grow' (basic-class2.js@2:21-30)")
+    'basic-class2.js:global:8 -> basic-class2.js:grow:2'
+    >>> format_output("'global' (require.js@3:98-159) -> 'console_log' (Native)")
+    'require.js:global:3 -> Native'
+    """
     if reg_native.match(out):
         return format_native(out)
     else:
