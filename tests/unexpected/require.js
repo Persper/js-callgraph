bb7de7175f55439dfa5b07bab9287ff7cf35325a
alexstennet <alexstennet@berkeley.edu>
2018-06-19 12:40:07 -0700

    Update unexpected/require.js to import to different name

--- tests/unexpected/require.js
+++ tests/unexpected/require.js
@@ -1,5 +1,5 @@
 // This file tests directly assigning a constructor function to module.exports
-let User = require('./require-module');
+let user = require('./require-module');
 
-let u = new User('random user', 'random-user@persper.org');
+let u = new user('random user', 'random-user@persper.org');
 console.log("%s's email is %s", u.name, u.email);
