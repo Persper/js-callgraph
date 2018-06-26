85876d74a1130151ea5101510d4d613f6a35129d
alexstennet <alexstennet@berkeley.edu>
2018-06-26 12:21:56 -0700

    Move require based tests to folder called import-export

--- tests/import-export/module-exports-require2.js
+++ tests/import-export/module-exports-require2.js
@@ -1,3 +1,3 @@
 // This file tests the most common usage of exports/require
-let m1 = require('./require-module');
+let m1 = require('./module-exports-module2');
 console.log("Adding %d to 10 gives us %d", m1.x, m1.addX(10));
