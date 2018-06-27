ce9b3909464c6bc75780773604e3d8a0d1e17d68
Hezheng Yin <yinhezheng1995@gmail.com>
2018-06-26 21:37:52 -0700

    Minor formatting changes

--- src/detectChange.js
+++ src/detectChange.js
@@ -11,22 +11,23 @@ define(function (require, exports) {
             start = a[0];
         else
             start = b[0];
-    
+
         if (a[1] <= b[1])
             end = a[1];
         else
             end = b[1];
-    
+
         if (start > end)
             return 0
         else
             return end - start + 1;
     }
-    
+
     function detectChange (addDels, funcs) {
+        // return empty object by default
         const adds = addDels['adds'], dels = addDels['dels'];
         const res = {};
-    
+
         // changeType should one of 'adds' or 'dels'
         function updateRes(funcName, numLines, changeType) {
             if (res.hasOwnProperty(funcName)){
@@ -37,26 +38,26 @@ define(function (require, exports) {
                 res[funcName][changeType] = numLines;
             }
         }
-    
+
         for (let i = 0; i < funcs.length; i++) {
             const fc = funcs[i];
             for (let j = 0; j < adds.length; j++) {
                 if (fc['range'][0] <= adds[j][0] && adds[j][0] <= fc['range'][1]) {
                     updateRes(fc.name, adds[j][1], 'adds');
                     break;
-                } 
-            } 
+                }
+            }
             for (let k = 0; k < dels.length; k++) {
                 const interLength = getIntersectedLength(fc['range'], dels[k]);
                 if (interLength > 0) {
                     updateRes(fc.name, interLength, 'dels');
                     break;
                 }
-            } 
+            }
         }
 
         for (const funcName in res) {
-            res[funcName] = res[funcName]['adds'] + res[funcName]['dels']; 
+            res[funcName] = res[funcName]['adds'] + res[funcName]['dels'];
         }
         return res;
     }
