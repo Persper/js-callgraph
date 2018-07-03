7e278fa714fbc154b9c32f21f98e94e404e0700b
Alex Stennet <alex@persper.org>
2018-07-03 12:59:56 -0700

    Add export from syntax to testing framework

--- tests/required_files.py
+++ tests/required_files.py
@@ -1,15 +1,28 @@
 import re
 from resolve_path import resolve_path, root
 
-reg = re.compile("require\(\'(.*)\'\)")
+reg1 = re.compile("require\(\'(.*)\'\)");
+reg2 = re.compile("import .* from \'(.*)\'");
+reg3 = re.compile("import \'(.*)\'(?! from)");
+reg4 = re.compile("export .* from \'(.*)\'");
 
 def get_requires(path):
     try:
         f = path.open()
         text = f.read()
         f.close()
-        matches = reg.findall(text)
-        lst = [m + ('.js.o' if 'vue' in str(path) else '.js') for m in matches]
+        matches1 = reg1.findall(text)
+        matches2 = reg2.findall(text)
+        matches3 = reg3.findall(text)
+        matches4 = reg4.findall(text)
+
+        lst1 = [m + ('.js.o' if 'vue' in str(path) else '.js') for m in matches1]
+        lst2 = [m + ('.js.o' if 'vue' in str(path) else '.js') for m in matches2]
+        lst3 = [m + ('.js.o' if 'vue' in str(path) else '.js') for m in matches3]
+        lst4 = [m + ('.js.o' if 'vue' in str(path) else '.js') for m in matches4]
+
+        lst = lst1 + lst2 + lst3 + lst4
+        
         return lst
     except:
         return []
@@ -31,5 +44,4 @@ def collect_requires(path):
             rp = resolve_path(rf, uf.parent)
             if rp and rp.exists():
                 unread_files.append(rp.resolve())
-
     return read_files
