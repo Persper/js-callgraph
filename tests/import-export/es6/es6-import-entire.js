2f2f1d2e069ec668cd387bf8f8751e8de154a404
Alex Stennet <alex@persper.org>
2018-07-03 12:54:58 -0700

    Add es6 import-export tests

---
+++ tests/import-export/es6/es6-import-entire.js
@@ -0,0 +1,3 @@
+import * as lib from 'es6-export-fns';
+console.log(lib.square(11)); // 121
+console.log(lib.diag(4, 3)); // 5
---
+++ tests/import-export/es6/redirect/auth.js
@@ -0,0 +1,8 @@
+function login(x) {
+  return x;
+}
+
+function logout(x, y) {
+  return x + y;
+}
+export { login, logout };
