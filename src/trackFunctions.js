fdfd0503a30d47862f014083b86b10f9b5676a3f
Hezheng Yin <yinhezheng1995@gmail.com>
2018-07-06 18:59:30 -0700

    Use best fit instead of first fit for levenshtein heuristic

--- src/trackFunctions.js
+++ src/trackFunctions.js
@@ -4,30 +4,50 @@ function trackFunctions(forwardFuncs, bckwardFuncs, threshold=0.5) {
     const idMap = {};
     for (let ffunc of forwardFuncs) {
         let isHandled = false;
-        // heuristic 1: function name
+
+        // Basic: compare cf
+        for (let bfunc of bckwardFuncs) {
+            // Found ffunc's corresponding bfunc
+            // ffunc's cf doesn't change
+            if (ffunc['cf'] === bfunc['cf']) {
+                isHandled = true;
+                break
+            }
+        }
+        if (isHandled)
+            continue;
+
+        // If execution reaches here, then ffunc's cf is changed
+        // Heuristic 1: function name
         for (let bfunc of bckwardFuncs) {
             if (ffunc['name'] && ffunc['name'] === bfunc['name']) {
-                if (ffunc['cf'] !== bfunc['cf'])
-                    idMap[ffunc['cf']] = bfunc['cf'];
+                idMap[ffunc['cf']] = bfunc['cf'];
                 isHandled = true;
                 break;
             }
         }
-
         if (isHandled)
             continue;
 
-        // heuristic 2: enclosing function & levenshtein distance
+        // Heuristic 2: enclosing function & levenshtein distance
+        // Set lowestRatio to 1000 should be sufficiently large
+        let bestFitCf = null, lowestRatio = 1000;
         for (let bfunc of bckwardFuncs) {
-            // enclosing function
-            if (ffunc['encFunc'] === bfunc['encFunc']) {
-                // levenshtein
-                let d = levenshtein(ffunc['code'], bfunc['code']);
-                if (d / ffunc['code'].length < threshold)
-                    if (ffunc['cf'] !== bfunc['cf'])
-                        idMap[ffunc['cf']] = bfunc['cf'];
+            // enclosing function heuristic
+            // Stop considering bfunc if encFuncs are different
+            if (ffunc['encFunc'] !== bfunc['encFunc'])
+                continue
+
+            // levenshtein heuristic
+            let d = levenshtein(ffunc['code'], bfunc['code']);
+            let ratio = d / ffunc['code'].length;
+            if (ratio < threshold && ratio < lowestRatio) {
+                lowestRatio = ratio;
+                bestFitCf = bfunc['cf'];
             }
         }
+        if (bestFitCf)
+            idMap[ffunc['cf']] = bestFitCf;
     }
     return idMap;
 }
