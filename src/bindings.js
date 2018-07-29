ef302c6c387a7a630b63fe4248f72e0f5626a7e2
Hezheng Yin <hezheng@persper.org>
2018-07-29 10:33:24 -0700

    Add support for nested destructuring in function params

--- src/bindings.js
+++ src/bindings.js
@@ -49,41 +49,26 @@ define(function (require, exports) {
                             visit(nd.id);
                         }
                         // Put params into symbol table
-                        decl_scope.set('this', { type: 'Identifier',
-                            name: 'this',
-                            loc: nd.loc,
-                            range: nd.range,
-                            attr: { enclosingFile: nd.attr.enclosingFile,
-                                scope: decl_scope } });
+                        decl_scope.set('this',
+                            {
+                                type: 'Identifier',
+                                name: 'this',
+                                loc: nd.loc,
+                                range: nd.range,
+                                attr: {
+                                    enclosingFile: nd.attr.enclosingFile,
+                                    scope: decl_scope
+                                }
+                            }
+                        );
                         for (var i = 0; i < nd.params.length; ++i) {
                             // Handle identifer as before
                             if (nd.params[i].type === 'Identifier')
                                 decl_scope.set(nd.params[i].name, nd.params[i]);
 
-                            // ES6 Object Destructuring
-                            // Currently don't support rest and default params
-                            if (nd.params[i].type === 'ObjectPattern') {
-                                for (let prop of nd.params[i].properties) {
-                                    decl_scope.set(prop.value.name, prop.value);
-                                    if (prop.value.type !== 'Identifier')
-                                        console.log('WARNING: check FunctionParam/ObjectPattern case in bindings.js.');
-                                }
-                            }
-
-                            // ES6 Array Destructurin
-                            // Currently don't support rest and default params
-                            if (nd.params[i].type === 'ArrayPattern') {
-                                for (let elm of nd.params[i].elements) {
-                                    // Array destructuring can ignore some values, so check null first
-                                    if (elm) {
-                                        if (elm.type === 'Identifier')
-                                            decl_scope.set(elm.name, elm);
-                                        else
-                                            console.log('WARNING: check VariableDeclarator/ArrayPattern case in bindings.js.');
-                                    }
-                                }
-                            }
-
+                            // Always visit nd.params[i]
+                            // Case 1: If nd.params[i] is an Identifer, visit it to set its scope attribute
+                            // Case 2: If nd.params[i] is not an Identifer, visit it to set decl_scope
                             visit(nd.params[i]);
                         }
 
@@ -119,41 +104,62 @@ define(function (require, exports) {
                         break;
 
                     case 'MemberExpression':
+                        /*
+                        If computed === true,
+                        the node corresponds to a computed e1[e2] expression
+                        and property is an Expression.
+                        If computed === false,
+                        the node corresponds to a static e1.x expression
+                        and property is an Identifier.
+                        */
                         visit(nd.object);
                         if (nd.computed)
                             visit(nd.property);
                         return false;
 
                     case 'VariableDeclarator':
-                        // Re-declaration of a variable in the same scope is ignored in JS
+                        // If nd.id is an Identifer and its name hasn't been declared in the scope, set its name in the scope
+                        // Re-declaration of a variable in the same scope is ignored
                         if (nd.id.type === 'Identifier' && !decl_scope.hasOwn(nd.id.name))
                             decl_scope.set(nd.id.name, nd.id);
 
-                        // ES6 Object Destructuring
-                        // Currently don't support rest and default params
-                        if (nd.id.type === 'ObjectPattern') {
-                            for (let prop of nd.id.properties) {
-                                decl_scope.set(prop.value.name, prop.value);
-                                if (prop.value.type !== 'Identifier')
-                                    console.log('WARNING: check VariableDeclarator/ObjectPattern case in bindings.js.');
-                            }
-                        }
-
-                        // ES6 Array Destructurin
-                        // Currently don't support rest and default params
-                        if (nd.id.type === 'ArrayPattern') {
-                            for (let elm of nd.id.elements) {
-                                // Array destructuring can ignore some values, so check null first
-                                if (elm) {
-                                    if (elm.type === 'Identifier')
-                                        decl_scope.set(elm.name, elm);
-                                    else
-                                        console.log('WARNING: check VariableDeclarator/ArrayPattern case in bindings.js.');
-                                }
-                            }
-                        }
+                        // break to visit nd.id and nd.init
+                        // Case 1: If nd.id is an Identifer, visit it and set its scope attribute
+                        // Case 2: If nd.id is not an Identifer ('ObjectPattern' or 'ArrayPattern'),
+                        //          visit it to set decl_scope
                         break;
+                    case 'ObjectPattern':
+                        // ES6 Object Destructuring
+                        // { key: value }
+                        // The newly declared name is always in value
+                        // Haven't tested with rest and default params
+                        for (let prop of nd.properties) {
+                            // This solution has a bug:
+                            // Suppose this 'ObjectPattern' unpacks the value into a global variable
+                            // decl_scope won't have the global variable name as its own property
+                            // thus, we would set its name in decl_scope, which is a mistake
+                            // For code example, please see tests/es6/binding-pattern-global.js
+                            if (prop.value.type === 'Identifier' && !decl_scope.hasOwn(prop.value.name))
+                                decl_scope.set(prop.value.name, prop.value);
 
+                            visit(prop.value);
+                        }
+                        return false;
+                    case 'ArrayPattern':
+                        // ES6 Array Destructuring
+                        // Haven't tested with rest and default params
+                        for (let elm of nd.elements) {
+                            // Array destructuring can ignore some values, so check null first
+                            if (elm) {
+                                // This solution has a bug, please see 'ObjectPattern' above
+                                // For code example, please see tests/es6/binding-pattern-global.js
+                                if (elm.type === 'Identifier' && !decl_scope.hasOwn(elm.name))
+                                    decl_scope.set(elm.name, elm);
+
+                                visit(elm);
+                            }
+                        }
+                        return false;
                     case 'Property':
                         // don't visit nd.key
                         visit(nd.value);
