bdc59f82712272ddde333c35a99d422603157101
Jinglei Ren <jinglei@persper.org>
2018-08-03 10:20:54 +0800

    Add copyright notice of Persper

--- src/harness.js
+++ src/harness.js
@@ -1,5 +1,7 @@
 /*******************************************************************************
- * Copyright (c) 2013 Max Schaefer.
+ * Copyright (c) 2013 Max Schaefer
+ * Copyright (c) 2018 Persper Foundation
+ *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
