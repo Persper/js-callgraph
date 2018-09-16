d72d6fe53e8e9ce2034ae72101a8ea1079d6b633
alexstennet <alex@persper.org>
2018-09-15 22:37:49 -0700

    Remove unnecessary native based computation

--- tests/process.py
+++ tests/process.py
@@ -45,61 +45,22 @@ def precision_recall(test_file, expected_output=None, display=False):
     # Reading in expected output file and comparing with output
     f = open(expected_output)
 
-    lines = [line[:-1] for line in f]
+    lines = [line[:-1] for line in f if 'Native' not in line]
 
     output_lines = set(fo)
     expected_lines = set(lines)
 
     intersection = output_lines & expected_lines
 
-    output_no_natives = [l for l in fo if 'Native' not in l and
-                                   '_interopRequireDefault' not in l and
-                                   l[-1] != '>']
-    expected_no_natives = [l for l in lines if 'Native' not in l and
-                                      '_interopRequireDefault' not in l and
-                                      l[-1] != '>']
+    if len(output_lines) > 0:
+        precision = 100 * len(intersection) // len(output_lines)
+    else:
+        precision = 0
 
-    output_set_wo_natives = set(output_no_natives)
-    expected_set_wo_natives = set(expected_no_natives)
+    recall = 100*len(intersection) // len(expected_lines)
 
-    interesection_wo_natives = output_set_wo_natives & expected_set_wo_natives
+    return precision, recall
 
-    if display:
-        print('Actual Output (w/o natives)')
-        for f in output_no_natives:
-            print(f)
-        print()
-        print('Expected Output (w/o natives)')
-        for f in expected_no_natives:
-            print(f)
-        print()
-        print('INCLUDING NATIVES')
-        print('Precision:', 100*len(intersection) // len(output_lines), '% (', len(intersection), '/', len(output_lines), ')')
-        print('Recall: ', 100*len(intersection) // len(expected_lines), '% (', len(intersection), '/', len(expected_lines), ')')
-        print()
-        print('EXCLUDING NATIVES')
-        print('Precision:', 100*len(interesection_wo_natives) // len(output_set_wo_natives),
-              '% (', len(interesection_wo_natives), '/', len(output_set_wo_natives), ')')
-        print('Recall: ', 100*len(interesection_wo_natives) // len(expected_set_wo_natives),
-              '% (', len(interesection_wo_natives), '/', len(expected_set_wo_natives), ')')
-
-    w_natives_precision = 100*len(intersection) // (len(output_lines) if len(output_lines) > 0 else -1)
-    w_natives_recall = 100*len(intersection) // (len(expected_lines) if len(expected_lines) > 0 else -1)
-    w_natives_intersection = len(intersection)
-    w_natives_output = len(output_lines)
-    w_natives_expected = len(expected_lines)
-
-    w_natives = (w_natives_precision, w_natives_recall, w_natives_intersection, w_natives_output, w_natives_expected)
-
-    wo_natives_precision = 100*len(interesection_wo_natives) // (len(output_set_wo_natives) if len(output_set_wo_natives) > 0 else -1)
-    wo_natives_recall = 100*len(interesection_wo_natives) // (len(expected_set_wo_natives) if len(expected_set_wo_natives) > 0 else -1)
-    wo_natives_intersection = len(interesection_wo_natives)
-    wo_natives_output = len(output_set_wo_natives)
-    wo_natives_expected = len(expected_set_wo_natives)
-
-    wo_natives = (wo_natives_precision, wo_natives_recall, wo_natives_intersection, wo_natives_output, wo_natives_expected)
-
-    return w_natives, wo_natives
 
 if __name__ == "__main__":
     assert len(sys.argv) == 3, "Incorrect number of arguments: process.py FILENAME TEST_FILE"
